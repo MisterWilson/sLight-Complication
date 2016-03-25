cd1f67483ed55108735ca2826df1283cedca4fe4
MisterWilson <dmrifles@gmail.com>
2016-03-24 17:40:29 -0700

    Initial Commit

---
+++ sLight Complication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sLight Complication
+//
+//  Created by Blaine Wilson on 3/24/16.
+//  Copyright © 2016 Blaine Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
